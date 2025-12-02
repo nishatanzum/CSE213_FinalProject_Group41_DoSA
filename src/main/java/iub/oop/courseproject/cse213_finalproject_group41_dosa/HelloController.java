f5ecbdb6ecf19c8f80b22f543827d1858d8dbdd0
nishatanzum <nishatanzumkatha@gmail.com>
2025-12-02 15:23:10 +0600

    Initial Commit

---
+++ src/main/java/iub/oop/courseproject/cse213_finalproject_group41_dosa/HelloController.java
@@ -0,0 +1,14 @@
+package iub.oop.courseproject.cse213_finalproject_group41_dosa;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module iub.oop.courseproject.cse213_finalproject_group41_dosa {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens iub.oop.courseproject.cse213_finalproject_group41_dosa to javafx.fxml;
+    exports iub.oop.courseproject.cse213_finalproject_group41_dosa;
+}
